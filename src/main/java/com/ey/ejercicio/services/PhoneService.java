22f81a2195d6cac37d76a0cbe8a52735f7878970
Agustin Zammarrelli <Agustin.Zammarrelli@ar.ey.com>
2023-03-13 18:39:40 -0300

    Se agrga documentaciones a services

--- src/main/java/com/ey/ejercicio/services/PhoneService.java
+++ src/main/java/com/ey/ejercicio/services/PhoneService.java
@@ -7,30 +7,57 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Servicio para manejar los teléfonos
+ */
 @Service
 public class PhoneService {
 
     @Autowired
     private PhoneDao phoneDao;
 
+    /**
+     * Guarda un teléfono en la base de datos
+     * @param phone el teléfono a guardar
+     * @return el teléfono guardado
+     */
     public Phone savePhone(Phone phone) {
         phoneDao.save(phone);
         return phone;
     }
 
+    /**
+     * Actualiza un teléfono en la base de datos
+     * @param phone el teléfono a actualizar
+     * @return el teléfono actualizado
+     */
     public Phone updatePhone(Phone phone) {
         phoneDao.update(phone);
         return phone;
     }
 
+    /**
+     * Obtiene un teléfono por su id
+     * @param id el id del teléfono a obtener
+     * @return el teléfono con el id especificado
+     */
     public Phone getPhoneById(Long id) {
         return phoneDao.findById(id);
     }
 
+    /**
+     * Obtiene una lista de teléfonos por el id de usuario
+     * @param userId el id del usuario cuyos teléfonos se quieren obtener
+     * @return una lista de teléfonos pertenecientes al usuario especificado
+     */
     public List<Phone> getPhonesByUserId(Long userId) {
         return phoneDao.findByUserId(userId);
     }
 
+    /**
+     * Elimina un teléfono de la base de datos
+     * @param phone el teléfono a eliminar
+     */
     public void deletePhone(Phone phone) {
         phoneDao.delete(phone);
     }
--- src/main/java/com/ey/ejercicio/services/UserService.java
+++ src/main/java/com/ey/ejercicio/services/UserService.java
@@ -1,7 +1,6 @@
 package com.ey.ejercicio.services;
 
 import com.ey.ejercicio.daos.UserDao;
-import com.ey.ejercicio.daosImpl.UserDaoImpl;
 import com.ey.ejercicio.dtos.UserDTO;
 import com.ey.ejercicio.entities.User;
 import com.ey.ejercicio.utils.EncryptionUtils;
@@ -14,6 +13,9 @@ import org.springframework.stereotype.Service;
 import javax.persistence.PersistenceException;
 import java.util.Date;
 
+/**
+ * Servicio para manejar los usuarios
+ */
 @Service
 public class UserService {
 
@@ -25,41 +27,70 @@ public class UserService {
 
     private static final Logger logger = LogManager.getLogger(UserService.class);
 
-    public User saveUser(UserDTO userDTO) {
+    /**
+     * Guarda un usuario en la base de datos
+     * @param userDTO el DTO del usuario a guardar
+     * @return el usuario guardado
+     * @throws PersistenceException si el correo del usuario ya está registrado
+     */
+    public User saveUser(UserDTO userDTO) throws PersistenceException {
 
+        // Verifica si el correo electrónico del usuario ya existe en la base de datos
         if (userDao.findByEmail(userDTO.getEmail()) != null) {
             logger.info("Usuario ya registrado");
             throw new PersistenceException("El correo ya está registrado");
         }
 
+        // Crea un nuevo objeto User a partir del objeto UserDTO proporcionado
         User user = new User();
         user = user.mapToUser(userDTO);
 
+        // Encripta la contraseña del usuario y crea un token JWT
         String encryptedPassword = user.getPassword();
         String token = jwtUtil.create(user.getName(), user.getEmail());
-
         user.setPassword(EncryptionUtils.encrypt(encryptedPassword));
         user.setToken(token);
 
-            userDao.save(user);
-            return user;
+        // Guarda el objeto User en la base de datos
+        userDao.save(user);
+        return user;
+
 
     }
 
+    /**
+     * Actualiza un usuario en la base de datos
+     * @param user el usuario a actualizar
+     * @return el usuario actualizado
+     */
     public User updateUser(User user) {
         user.setModified(new Date());
         userDao.update(user);
         return user;
     }
 
+    /**
+     * Obtiene un usuario por su id
+     * @param id el id del usuario a obtener
+     * @return el usuario con el id especificado
+     */
     public User getUserById(Long id) {
         return userDao.findById(id);
     }
 
+    /**
+     * Obtiene un usuario por su correo electrónico
+     * @param email el correo electrónico del usuario a obtener
+     * @return el usuario con el correo electrónico especificado
+     */
     public User getUserByEmail(String email) {
         return userDao.findByEmail(email);
     }
 
+    /**
+     * Elimina un usuario de la base de datos
+     * @param id el id del usuario a eliminar
+     */
     public void deleteUser(Long id) {
         User user = userDao.findById(id);
         user.setIsActive(false);
